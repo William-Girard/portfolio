0992dc073c1276f47fa7f337b963e12d0aec244b
William <w.girard@codeur.online>
2021-02-24 13:32:29 +0100

    portfolio

---
+++ anime.js
@@ -0,0 +1,55 @@
+/*let click = document.getElementsByTagName("body");
+click.addEventListener("click", anime);*/
+
+let logo = document.getElementById('logo');
+/*animation.addEventListener('complete', function () {
+    logo.addEventListener('click', function () {
+      anime.goToAndStop(0);
+      anime.play();
+    }, { once: true });
+  });*/
+logo.addEventListener('click', function () {
+    animation.restart();
+});
+
+let animation = anime.timeline({loop: false})
+.add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+}).add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+}).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+}).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+}).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+}).add({
+    targets: '.ml5',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
+
